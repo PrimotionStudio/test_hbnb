69ad547333841fd668f5dbe7b6015acd1544c432
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-04 23:16:26 +0100

    0

--- hbnb/console.py
+++ hbnb/console.py
@@ -7,13 +7,14 @@ import cmd
 # import signal
 from models.base_model import BaseModel
 from models import storage
+from pprint import pprint
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
     __classes = ["BaseModel"]
 
-    def emptyline(self, hbnb):
+    def emptyline(self):
         pass
 
     def do_EOF(self, hbnb):
@@ -23,6 +24,7 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_create(self, hbnb):
+        hbnb = hbnb.strip()
         if hbnb:
             if hbnb in HBNBCommand.__classes:
                 new_bm = BaseModel(hbnb)
@@ -34,6 +36,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
     def do_show(self, hbnb):
+        hbnb = hbnb.strip()
         cmd_args = hbnb.split(" ")
         if not cmd_args[0]:
             print("** class name missing **")
@@ -47,6 +50,7 @@ class HBNBCommand(cmd.Cmd):
             print(storage.all()["<{}>.{}".format(cmd_args[0], cmd_args[1])])
 
     def do_destroy(self, hbnb):
+        hbnb = hbnb.strip()
         cmd_args = hbnb.split(" ")
         if not cmd_args[0]:
             print("** class name missing **")
@@ -60,6 +64,21 @@ class HBNBCommand(cmd.Cmd):
             del storage.all()["<{}>.{}".format(cmd_args[0], cmd_args[1])]
             storage.save()
 
+    def do_all(self, hbnb):
+        """Print all instances of all or acertain class."""
+        hbnb = hbnb.strip()
+        obj_list = []
+        if hbnb:
+            if hbnb in HBNBCommand.__classes:
+                for obj in storage.all().values():
+                    if isinstance(obj, globals()[hbnb]):
+                        obj_list.append(obj.__str__())
+        else:
+            for obj in storage.all().values():
+                obj_list.append(obj.__str__())
+
+        print(obj_list)
+
 
 def check_for_id(_id, obj_dict):
     for k, v in obj_dict.items():
@@ -70,7 +89,8 @@ def check_for_id(_id, obj_dict):
 # def ctrlc(sig, handle):
 #     sys.exit(0)
 
+# signal.signal(signal.SIGINT, ctrlc)
+
 
 if __name__ == "__main__":
-    # signal.signal(signal.SIGINT, ctrlc)
     HBNBCommand().cmdloop()
