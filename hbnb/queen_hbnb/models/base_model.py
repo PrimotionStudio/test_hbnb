cfd87bffd3983a6d6908e5bab9c80a56d076205e
Queendarlin <queendarlinnnamani@gmail.com>
2024-02-07 04:28:48 -0800

    Updated test_amenity, now OK

--- hbnb/queen_hbnb/models/base_model.py
+++ hbnb/queen_hbnb/models/base_model.py
@@ -36,7 +36,8 @@ class BaseModel:
         Returns:
             str: String representation
         """
-        return f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         """Update the public instance attribute 'updated_at'
